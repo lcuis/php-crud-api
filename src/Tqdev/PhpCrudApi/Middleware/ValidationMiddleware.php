5db7950a70c29a98f72f8e7ec9aed67582283400
louis <louis@jod.li>
2020-04-23 11:34:47 +0000

    merging

--- src/Tqdev/PhpCrudApi/Middleware/ValidationMiddleware.php
+++ src/Tqdev/PhpCrudApi/Middleware/ValidationMiddleware.php
@@ -7,39 +7,13 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Tqdev\PhpCrudApi\Column\ReflectionService;
 use Tqdev\PhpCrudApi\Column\Reflection\ReflectedTable;
-<<<<<<< HEAD
-=======
 use Tqdev\PhpCrudApi\Column\Reflection\ReflectedColumn;
->>>>>>> 3605953a927842c88dfbc873d105c964bec3011d
 use Tqdev\PhpCrudApi\Controller\Responder;
 use Tqdev\PhpCrudApi\Middleware\Base\Middleware;
 use Tqdev\PhpCrudApi\Middleware\Router\Router;
 use Tqdev\PhpCrudApi\Record\ErrorCode;
 use Tqdev\PhpCrudApi\RequestUtils;
 
-<<<<<<< HEAD
-class ValidationMiddleware extends Middleware {
-	private $reflection;
-	private $typesToValidate;
-
-	public function __construct(Router $router, Responder $responder, array $properties, ReflectionService $reflection) {
-		parent::__construct($router, $responder, $properties);
-		$this->reflection = $reflection;
-		$typesStr = $this->getProperty('types', 'all');
-		if (is_null($typesStr)) {
-			$typesStr = 'all';
-		}
-		if (strlen($typesStr) == 0) {
-			$typesStr = 'none';
-		}
-		$this->typesToValidate = explode(',', $typesStr);
-		if (is_null($this->typesToValidate) || count($this->typesToValidate) == 0) {
-			$this->typesToValidate = ['all'];
-		}
-	}
-
-	private function callHandler($handler, $record, string $operation, ReflectedTable $table) /*: ResponseInterface?*/ {
-=======
 class ValidationMiddleware extends Middleware
 {
 	private $reflection;
@@ -52,7 +26,6 @@ class ValidationMiddleware extends Middleware
 
 	private function callHandler($handler, $record, string $operation, ReflectedTable $table) /*: ResponseInterface?*/
 	{
->>>>>>> 3605953a927842c88dfbc873d105c964bec3011d
 		$context = (array) $record;
 		$details = array();
 		$tableName = $table->getName();
@@ -60,13 +33,8 @@ class ValidationMiddleware extends Middleware
 			if ($table->hasColumn($columnName)) {
 				$column = $table->getColumn($columnName);
 				$valid = call_user_func($handler, $operation, $tableName, $column->serialize(), $value, $context);
-<<<<<<< HEAD
-				if ($valid || $valid == '') {
-					$valid = $this->validateType($column->serialize(), $value);
-=======
 				if ($valid === true || $valid === '') {
 					$valid = $this->validateType($column, $value);
->>>>>>> 3605953a927842c88dfbc873d105c964bec3011d
 				}
 				if ($valid !== true && $valid !== '') {
 					$details[$columnName] = $valid;
@@ -79,142 +47,6 @@ class ValidationMiddleware extends Middleware
 		return null;
 	}
 
-<<<<<<< HEAD
-	private function validateType($column, $value) {
-		if ($this->typesToValidate[0] == 'none') {
-			return (true);
-		}
-		if ($this->typesToValidate[0] != 'all') {
-			if (!in_array($column['type'], $this->typesToValidate)) {
-				return (true);
-			}
-		}
-		if (is_null($value)) {
-			return ($column["nullable"] ? true : "cannot be null");
-		}
-		switch ($column['type']) {
-		case 'integer':
-			if (!is_numeric($value)) {
-				return ('must be numeric');
-			}
-
-			if (strlen($value) > 20) {
-				return ('exceeds range');
-			}
-
-			break;
-		case 'bigint':
-			if (!is_numeric($value)) {
-				return ('must be numeric');
-			}
-
-			if (strlen($value) > 20) {
-				return ('exceeds range');
-			}
-
-			break;
-		case 'varchar':
-			if (strlen($value) > $column['length']) {
-				return ('too long');
-			}
-
-			break;
-		case 'decimal':
-			if (!is_float($value) && !is_numeric($value)) {
-				return ('not a float');
-			}
-
-			break;
-		case 'float':
-			if (!is_float($value) && !is_numeric($value)) {
-				return ('not a float');
-			}
-
-			break;
-		case 'double':
-			if (!is_float($value) && !is_numeric($value)) {
-				return ('not a float');
-			}
-
-			break;
-		case 'boolean':
-			if ($value != 0 && $value != 1) {
-				return ('not a valid boolean');
-			}
-
-			break;
-		case 'date':
-			$date_array = explode('-', $value);
-			if (count($date_array) != 3) {
-				return ('invalid date format use yyyy-mm-dd');
-			}
-
-			if (!@checkdate($date_array[1], $date_array[2], $date_array[0])) {
-				return ('not a valid date');
-			}
-
-			break;
-		case 'time':
-			$time_array = explode(':', $value);
-			if (count($time_array) != 3) {
-				return ('invalid time format use hh:mm:ss');
-			}
-
-			foreach ($time_array as $t) {
-				if (!is_numeric($t)) {
-					return ('non-numeric time value');
-				}
-			}
-
-			if ($time_array[1] < 0 || $time_array[2] < 0 || $time_array[0] < -838 || $time_array[1] > 59 || $time_array[2] > 59 || $time_array[0] > 838) {
-				return ('not a valid time');
-			}
-
-			break;
-		case 'timestamp':
-			$split_timestamp = explode(' ', $value);
-			if (count($split_timestamp) != 2) {
-				return ('invalid timestamp format use yyyy-mm-dd hh:mm:ss');
-			}
-
-			$date_array = explode('-', $split_timestamp[0]);
-			if (count($date_array) != 3) {
-				return ('invalid date format use yyyy-mm-dd');
-			}
-
-			if (!@checkdate($date_array[1], $date_array[2], $date_array[0])) {
-				return ('not a valid date');
-			}
-
-			$time_array = explode(':', $split_timestamp[1]);
-			if (count($time_array) != 3) {
-				return ('invalid time format use hh:mm:ss');
-			}
-
-			foreach ($time_array as $t) {
-				if (!is_numeric($t)) {
-					return ('non-numeric time value');
-				}
-			}
-
-			if ($time_array[1] < 0 || $time_array[2] < 0 || $time_array[0] < 0 || $time_array[1] > 59 || $time_array[2] > 59 || $time_array[0] > 23) {
-				return ('not a valid time');
-			}
-
-			break;
-		case 'clob':
-			break;
-		case 'blob':
-			break;
-		case 'varbinary':
-			if (((strlen($value) * 3 / 4) - substr_count(substr($value, -2), '=')) > $column['length']) {
-				return ('too long');
-			}
-
-			break;
-		case 'geometry':
-			break;
-=======
 	private function validateType(ReflectedColumn $column, $value)
 	{
 		$types = $this->getArrayProperty('types', 'all');
@@ -357,17 +189,12 @@ class ValidationMiddleware extends Middleware
 					}
 					break;
 			}
->>>>>>> 3605953a927842c88dfbc873d105c964bec3011d
 		}
 		return (true);
 	}
 
-<<<<<<< HEAD
-	public function process(ServerRequestInterface $request, RequestHandlerInterface $next): ResponseInterface{
-=======
 	public function process(ServerRequestInterface $request, RequestHandlerInterface $next): ResponseInterface
 	{
->>>>>>> 3605953a927842c88dfbc873d105c964bec3011d
 		$operation = RequestUtils::getOperation($request);
 		if (in_array($operation, ['create', 'update', 'increment'])) {
 			$tableName = RequestUtils::getPathSegment($request, 2);
